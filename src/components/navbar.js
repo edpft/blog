04b29cd2505633a66b3b0724d820084920dcd9a8
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2021-04-09 14:23:54 +0100

    move dashboard to front page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 import { useStaticQuery, graphql } from "gatsby";
 import React from "react";
-import { Navbar, Button, Form, FormControl } from "react-bootstrap";
-import NavBarLinks from "./navbar-links";
+import { Navbar } from "react-bootstrap";
 
 export default function NavBar() {
   const data = useStaticQuery(
@@ -20,13 +19,6 @@ export default function NavBar() {
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="/">{data.site.siteMetadata.title}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <NavBarLinks />
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
     </Navbar>
   );
 }
