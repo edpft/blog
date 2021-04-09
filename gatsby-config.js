6b1ac090132ed79ad90f1da5fd1628f75b43c360
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2021-04-09 14:23:42 +0100

    add footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,5 @@
 module.exports = {
   siteMetadata: {
-    title: "DevOops",
-    footer: "This is my footer",
+    title: "UK GE Dashboard",
   },
-  plugins: [
-    "gatsby-plugin-emotion",
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        defaultLayouts: {
-          default: require.resolve("./src/components/dashboard.js"),
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
-    },
-    `gatsby-transformer-json`,
-  ],
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React from "react";
 
 export default function Footer(props) {
-    return (
-        <footer>
-            <span>{ props.footerText }</span>    
-        </footer>
-    )
+  return (
+    <footer>
+      <span>
+        Source:{" "}
+        <a href="https://www.electoralcalculus.co.uk/flatfile.html">
+          Historical Data and Plots, Electoral Calculus, 28 Feb. 2020
+        </a>
+      </span>
+    </footer>
+  );
 }
